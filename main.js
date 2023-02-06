b53eac9eba7304a62c06320be192dc4571d2e69c
이종경 <gyoung3063413@naver.com>
2023-02-06 22:01:34 +0900

    index 수정

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+$(".open-btn").click(function () {
+    $(this).css("display","none")
+    $("header").addClass("on")
+    $("header ul").css("display", "flex")
+    $(".close-btn").css("display", "block")
+})
+$(".close-btn").click(function () {
+    $(this).css("display","none")
+    $("header").removeClass("on")
+    $("header ul").css("display", "none")
+    $(".open-btn").css("display", "block")
+})
+//메뉴 열고닫기
+
 $(".color_option span").click(function (e) {
     $(".on").removeClass("on")
     $(this).addClass("on")
@@ -20,9 +34,10 @@ $(window).scroll(function () {
     }
 })
 
-var auto_height = parseInt($(".video_wrap li").css("width"))
-$(".video_wrap li").css("height", auto_height / 2)
-// li높이 고정
+var user_width = $(window).width() / 1.5;
+$(".video_wrap li").css("width", user_width)
+$(".video_wrap li").css("height", user_width * 9 / 16)
+// li크기 고정
 
 var li_copy = $(".video_wrap li").slice(0, 2).clone();
 $(".video_wrap").append(li_copy)
@@ -30,9 +45,13 @@ var user_w = $(".video_slide").width() / 2,
     li_length = $(".video_wrap li").length,
     li_gap = parseInt($(".video_wrap").css("gap")),
     li_width = $(".video_wrap li").width(),
+    li_height = $(".video_wrap li").height(),
     li_size = li_width + li_gap;
 var li_cent = user_w - (li_width / 2) - li_width - li_gap
 $(".video_wrap").css("marginLeft", li_cent)
+
+$("#vid_btn").css("width", li_width)
+// 버튼 크기 고정
 
 var n = 0;
 $("#vid_btn button").click(function () {
