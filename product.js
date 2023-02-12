5095b482a558eb5c4937a74073f6b1db2f6326a1
taegun1011 <xorjsghkd1011@gmail.com>
2023-02-12 21:58:49 +0900

    product 특정 섹션 nav 배경 제거

--- product.js
+++ product.js
@@ -16,6 +16,12 @@ $(window).scroll(function () {
     if (t > $(".process_2").offset().top - h) {
         $(".process_2").addClass("on")
     }
+    if (t > $(".process").offset().top && t < $(".product_2").offset().top) {
+        $("header").addClass("no-bg")
+    }
+    else {
+        $("header").removeClass("no-bg")
+    }
 })
 
 $(".color_option span").click(function (e) {
