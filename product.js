95112d375c89cd1f71c598122536bbcb1433b391
이종경 <gyoung3063413@naver.com>
2023-02-12 22:41:31 +0900

    product.js 수정

--- product.js
+++ product.js
@@ -17,17 +17,32 @@ $(window).scroll(function () {
         $(".process_2").addClass("on")
     }
 })
-
-$(".color_option span").click(function (e) {
-    $(".on").removeClass("on")
-    $(this).addClass("on")
-    if (e.target.dataset.color == "pink") {
-        $(".change_img").attr("src", "img/nawa_cup1.png")
-    }
-    if (e.target.dataset.color == "black") {
-        $(".change_img").attr("src", "img/nawa_cup1_black.png")
-    }
-    if (e.target.dataset.color == "gray") {
-        $(".change_img").attr("src", "img/nawa_cup1_gray.png")
-    }
-})
+if ($(window).width() > 768) {
+    $(".color_option span").click(function (e) {
+        $(".on").removeClass("on")
+        $(this).addClass("on")
+        if (e.target.dataset.color == "pink") {
+            $(".change_img").attr("src", "img/nawa_cup1.png")
+        }
+        if (e.target.dataset.color == "black") {
+            $(".change_img").attr("src", "img/nawa_cup1_black.png")
+        }
+        if (e.target.dataset.color == "gray") {
+            $(".change_img").attr("src", "img/nawa_cup1_gray.png")
+        }
+    })
+} else {
+    $(".color_option span").click(function (e) {
+        $(".on").removeClass("on")
+        $(this).addClass("on")
+        if (e.target.dataset.color == "pink") {
+            $(".nawa_product").css("background", "no-repeat center /contain url(img/nawa_cup1.png), #f5f5f5")
+        }
+        else if (e.target.dataset.color == "black") {
+            $(".nawa_product").css("background", "no-repeat center /contain url(img/nawa_cup1_black.png), #f5f5f5")
+        }
+        else if (e.target.dataset.color == "gray") {
+            $(".nawa_product").css("background", "no-repeat center /contain url(img/nawa_cup1_gray.png), #f5f5f5")
+        }
+    })
+}
